b654cdb7ac853c39f6e6ac2aa20f722f6ffd0857
Ben Greene <bgree052@gold.ac.uk>
2017-11-07 20:17:57 +0000

    Finished Invert

--- sketch.js
+++ sketch.js
@@ -1,15 +1,48 @@
-var img;
+var colourImg;
+var monoImg;
+var invertImg;
+var inc=0;
+var cout = true;
 
 function preload(){
-  img = loadImage("assets/colour3.jpg");
+  colourImg = loadImage("assets/colour3.jpg");
+  monoImg = loadImage("assets/colour3.jpg");
+  invertImg = loadImage("assets/colour3.jpg");
 }
 
 function setup(){
   createCanvas(600,400);
-  img.loadPixels();
-  //pixelDensity(1);
+  colourImg.loadPixels();
+  monoImg.loadPixels();
+  invertImg.loadPixels();
 }
 
 function draw(){
-  image(img, 0 , 0, 100,200);
+  if(inc==0){
+    image(colourImg, 0 , 0, 600,400);
+  }
+  if(inc==1){
+    image(monoImg,0,0,600,400);
+    monoImg.filter("gray");
+  }
+  if(inc==2){
+    image(invertImg,0,0,600,400);
+    if(cout==true){
+      invertImg.filter(INVERT);
+    }
+    cout=false;
+  }
+}
+
+function keyPressed(){
+  console.log(key);
+  if(key == 'D'){
+    if(inc == 2){
+      inc=0
+    }
+    else{
+      inc +=1;
+    }
+    console.log(inc);
+  }
 }
